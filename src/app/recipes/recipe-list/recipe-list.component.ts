c962be4e15c10e916b9d62f85b7b70f7d4641efe
Andres Alcocer <andresalcocer7@yahoo.com>
2018-05-28 17:54:58 -0400

    recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model"; 
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  // variables
+  recipes: Recipe[] = [
+    // creating a new Recipe object
+    new Recipe("Test Recipe", "This is just a test", "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg")
+  ]; 
 
   constructor() { }
 
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+// Data model for a recipe
+export class Recipe {
+    public name: string; 
+    public description: string; 
+    public imgPath: string; 
+
+    constructor(name: string, desc: string, imgPath: string) {
+        this.name = name; 
+        this.description = desc; 
+        this.imgPath = imgPath; 
+    }
+}
